ed207f2d77013bc70b96eefe522143e2b4d875ff
Umar <nosacostra2001@gmail.com>
2025-02-25 16:56:05 +0500

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+
+      'xl': '1199px',
+      // => @media (min-width: 1280px) { ... }
+
+    }
+  },
+  plugins: [],
+}
+
